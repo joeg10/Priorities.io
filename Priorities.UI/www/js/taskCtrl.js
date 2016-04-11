148cfdfa8552d00f0817ebd994f8bca30cf4a594
Joe Gloeckner <joe.gloeckner@rminsight.net>
2016-04-11 12:53:56 -0400

    Priorities.io

---
+++ Priorities.UI/www/js/taskCtrl.js
@@ -0,0 +1,21 @@
+﻿angular.module('taskApp', [])
+  .controller('taskCtrl', function () {
+      var vm = this;
+      vm.addTaskText = '';
+      vm.task = [
+        { "tasktext": "Fix driveway" },
+        { "tasktext": "Replace roof" },
+        { "tasktext": "Fix dryer" },
+        { "tasktext": "Tear out deck" },
+        { "tasktext": "Add electricity to garage" }
+      ];
+      vm.addTask = function () {
+          if (vm.addTaskText !== '') {
+              vm.task.push({ "tasktext": vm.addTaskText });
+              vm.addTaskText = '';
+          }
+      }
+      vm.deleteTask = function (task) {
+          vm.task.splice(vm.task.indexOf(task), 1);
+      }
+  });
