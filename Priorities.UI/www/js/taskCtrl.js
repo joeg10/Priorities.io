4736e35a6ffd0227125b943bee6c79b02fa45097
Joe Gloeckner <joe.gloeckner@rminsight.net>
2016-05-19 13:01:14 -0400

    added grid

--- Priorities.UI/www/js/taskCtrl.js
+++ Priorities.UI/www/js/taskCtrl.js
@@ -2,6 +2,8 @@
   .controller('taskCtrl', function () {
       var vm = this;
       vm.addTaskText = '';
+      vm.display = 'list';
+
       vm.task = [
         { "task_name": "Fix driveway", "due_date": "1/1/2016" },
         { "task_name": "Replace roof", "due_date": "1/1/2016" },
@@ -17,5 +19,14 @@
       }
       vm.deleteTask = function (task) {
           vm.task.splice(vm.task.indexOf(task), 1);
+      }
+
+      vm.changeDisplay = function () {
+          if (vm.display === 'list') {
+              vm.display = 'grid';
+          }
+          else {
+              vm.display = 'list';
+          }
       }
   });
