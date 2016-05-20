8ccd1fda9005008f255d80dba66c99418bcb3117
Joe Gloeckner <joe.gloeckner@rminsight.net>
2016-05-20 07:51:52 -0400

    gridster

--- Priorities.UI/www/js/taskCtrl.js
+++ Priorities.UI/www/js/taskCtrl.js
@@ -1,19 +1,37 @@
-﻿angular.module('taskApp', [])
-  .controller('taskCtrl', function () {
+﻿var app = angular.module("taskApp", ['gridster', 'xeditable']);
+
+app.run(function (editableOptions) {
+    editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+});
+
+//angular.module('taskApp', ['gridster', 'xeditable'])
+app
+  .controller('taskCtrl', function ($scope, $log) {
       var vm = this;
       vm.addTaskText = '';
-      vm.display = 'list';
+      vm.display = 'grid';
+      $scope.animationsEnabled = true;
+
+      $scope.gridsterOptions = {
+          margins: [20, 20],
+          columns: 4,
+          draggable: {
+              handle: 'h3'
+          }
+      };
 
       vm.task = [
-        { "task_name": "Fix driveway", "due_date": "1/1/2016" },
-        { "task_name": "Replace roof", "due_date": "1/1/2016" },
-        { "task_name": "Fix dryer", "due_date": "1/1/2016" },
-        { "task_name": "Tear out deck", "due_date": "1/15/2016" },
-        { "task_name": "Add electricity to garage", "due_date": "1/3/2016" }
+        { "title": "Fix driveway", "description": "task description", "create_date": "1/1/2016", "due_date": "1/1/2016", "start_date": "1/1/2016", "complete_date": "1/1/2016", "priority": 1, status: "queued" },
+        { "title": "Replace roof", "description": "task description", "create_date": "1/1/2016", "due_date": "1/1/2016", "start_date": "1/1/2016", "complete_date": "1/1/2016", "priority": 2, status: "queued" },
+        { "title": "Fix dryer", "description": "task description", "create_date": "1/1/2016", "due_date": "1/1/2016", "start_date": "1/1/2016", "complete_date": "1/1/2016", "priority": 3, status: "queued" },
+        { "title": "Tear out deck", "description": "task description", "create_date": "1/1/2016", "due_date": "1/1/2016", "start_date": "1/1/2016", "complete_date": "1/1/2016", "priority": 4, status: "queued" },
+        { "title": "Add electricity to garage", "description": "task description", "create_date": "1/1/2016", "due_date": "1/1/2016", "start_date": "1/1/2016", "complete_date": "1/1/2016", "priority": 5, status: "queued" }
       ];
+
+
       vm.addTask = function () {
           if (vm.addTaskText !== '') {
-              vm.task.push({ "task_name": vm.addTaskText });
+              vm.task.push({ "title": vm.addTaskText });
               vm.addTaskText = '';
           }
       }
@@ -29,4 +47,49 @@
               vm.display = 'list';
           }
       }
+
+      vm.editItem = function (selectedTask) {
+          $scope.selectedTask = selectedTask;
+          $scope.open('lg');
+      }
+
+
+      ////modal section
+      //$scope.open = function (size) {
+
+      //    var modalInstance = $uibModal.open({
+      //        animation: $scope.animationsEnabled,
+      //        templateUrl: 'editModal.html',
+      //        controller: 'editModalCtrl',
+      //        size: size,
+      //        resolve: {
+      //            selectedTask: function () {
+      //                return $scope.selectedTask;
+      //            }
+      //        }
+      //    });
+
+      //    modalInstance.result.then(function (selectedItem) {
+      //       // vm.selected = selectedItem;
+      //    }, function () {
+      //        $log.info('Modal dismissed at: ' + new Date());
+      //    });
+      //};
+
   });
+
+//angular.module('taskApp').controller('editModalCtrl', function ($scope, $uibModalInstance, selectedTask) {
+
+//    $scope.selectedTask = selectedTask;
+//    //$scope.selected = {
+//    //    item: $scope.items[0]
+//    //};
+
+//    $scope.ok = function () {
+//        $uibModalInstance.close(selectedTask);
+//    };
+
+//    $scope.cancel = function () {
+//        $uibModalInstance.dismiss('cancel');
+//    };
+//});
