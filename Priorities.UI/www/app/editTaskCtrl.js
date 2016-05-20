8ed2564fb06bbd976c27844c5d26bdcc2baf4abb
Joe Gloeckner <joe.gloeckner@rminsight.net>
2016-05-20 11:35:04 -0400

    added edit module - nav not working

---
+++ Priorities.UI/www/app/editTaskCtrl.js
@@ -0,0 +1,16 @@
+﻿
+
+(function () {
+    'use strict';
+
+
+    angular.module('taskApp').controller('editTaskCtrl', ['$scope', editTask]);
+
+    function editTask($scope) {
+        alert('test');
+
+      
+    }
+
+})();
+
